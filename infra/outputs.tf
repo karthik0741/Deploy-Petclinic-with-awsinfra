6ae007c5139e3d45e4e59a75a877cc45a78ff14f
karthik0741 <sai.karthik0741@gmail.com>
2023-08-02 18:44:55 +0100

    new

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "application_load_balancer" {
+    value = module.application_load_balancer
+}
+output "security_group" {
+    value = module.security_group
+}
+output "ec2" {
+    value = module.ec2
+}
+output "rds" {
+    value = module.rds
+}
+output "vpc" {
+    value = module.vpc
+}
