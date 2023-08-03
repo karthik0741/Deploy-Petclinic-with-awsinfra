53149f9962179046a464a43bbe150a608687a94b
karthik0741 <sai.karthik0741@gmail.com>
2023-08-03 18:03:48 +0100

    udpate for rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -80,6 +80,6 @@ resource "aws_db_instance" "db_instance" {
   db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.database_security_group.id]
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
-  db_name                 = "applicationdb"
+  db_name                 = "petclinic"
   skip_final_snapshot     = true
 }
